7399e703e597b03922dd624df728c0214f1661d3
Echo <you@example.com>
2025-07-11 13:42:36 +0300

    Update README.md to provide a detailed setup guide for React with TypeScript and Vite, including ESLint configuration examples and recommended plugins.

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Home from './pages/Home';
+import { Layout } from './layout/Layout';
+// Create a client
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route element={<Layout />}>
+            {/* Public Routes */}
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/" element={<Login />} /> */}
+            {/* Protected Routes */}
+          </Route>
+          {/* Catch all route - outside of Layout */}
+          {/* <Route path="*" element={<NotFoundPage />} /> */}
+        </Routes>
+      </Router>
+    </QueryClientProvider>
+  );
+}
+
+export default App; 
